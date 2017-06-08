04cfb339d8f9e1f4f8e02b5bc0f5390beec7db50
Sigularusrex <djsigley@gmail.com>
2017-06-08 13:54:48 +0200

    ENV vars

--- app/bot/send_greeting.rb
+++ app/bot/send_greeting.rb
@@ -3,7 +3,7 @@
 
 require "facebook/messenger"
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["BOT_ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(access_token: Rails.application.secrets.bot_access_token)
 Bot.on :message do |message|
   puts "got your message!"
 end
