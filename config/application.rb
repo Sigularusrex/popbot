5a34d33c10a227d79a7ebcadfd79184e61f0f0cb
Sigularusrex <djsigley@gmail.com>
2017-06-08 13:07:14 +0200

    Facebook-messenger setup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Popbot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Auto-load the bot and its subdirectories
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
