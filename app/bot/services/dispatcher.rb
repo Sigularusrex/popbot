80917230f1adb9e5c16c5b63b6d680d0fb755ceb
Sigularusrex <djsigley@gmail.com>
2017-06-09 16:06:36 +0200

    comment

--- app/bot/services/dispatcher.rb
+++ app/bot/services/dispatcher.rb
@@ -1,5 +1,7 @@
 class Dispatcher
 
+  # Currently not used. I stubbed it out for future dev, but scope doesn't call for it just yet.
+  # Sending happens in the bot.rb file
   def initialize(bot, message)
     @message = message
     @bot = bot
